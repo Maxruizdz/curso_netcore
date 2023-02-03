6ed7e805f96c51ebf197eb2ede0ab7dc5a2dbe24
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-02-03 16:09:22 -0300

    creando cursos/objetos para nuestra escuela

---
+++ Entidades/Curso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_netCOREE.Entidades
+{
+    public class Curso
+    {
+        public string nombre { get;  set; }
+        public string UniqueID { get;private  set; }
+        public Tipos_Jornada tipos_Jornada { get; set; }
+
+        public Curso() {
+
+            UniqueID = Guid.NewGuid().ToString();
+        
+        
+        }
+    
+    
+    
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,3 +7,11 @@ Escuela escuela1 = new Escuela("Platzi Academy", 2023,
                        );
 escuela1.tipo_escuela = Tipo_Escuela.Secundaria;
 Escuela escuela2 = new Escuela("UTN", 2015, Tipo_Escuela.Universidad, pais: "Argentina");
+Curso curso1 = new Curso() {nombre="101" };
+Curso curso2 = new Curso() { nombre = "102" };
+Curso curso3 = new Curso() { nombre = "103" };
+
+
+Console.WriteLine(curso1.nombre+ "," + curso1.UniqueID);
+Console.WriteLine(curso2.nombre + ", " + curso2.UniqueID);
+Console.WriteLine(curso3.nombre + " ,"  +curso3.UniqueID);
